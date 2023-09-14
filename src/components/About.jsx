52ded969f4e4c40236b141436119b21b391fd9b7
Kazi <kazi98@proton.me>
2023-09-14 18:36:10 -0500

    added temmporary code to see if it is working

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import "./About.css";
 
 function About() {
-  return <h1 className="animated">Hello, I am a student</h1>;
+  return <h1 className="animated">Hello, Hey I am a student</h1>;
 }
 
 export default About;
