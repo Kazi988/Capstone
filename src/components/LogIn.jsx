0265f2da9dc0657d1849fcb4d25cd03cd50d860f
Kazi <kazi98@proton.me>
2023-09-21 18:13:47 -0500

    login jwt token figured out

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -13,11 +13,17 @@ function LogIn() {
     try {
       const result = await fetch("https://fakestoreapi.com/auth/login", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
           username: "mor_2314",
           password: "83r5^_",
         }),
       });
+      console.log(result);
+      const response = await result.json();
+      console.log(response);
     } catch (error) {
       console.error(error);
     }
